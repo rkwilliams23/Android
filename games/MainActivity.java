16eef63adcb1e2d85a64d18fd2f050d8a263c2f2
rkwilliams23 <rk.williams23@icloud.com>
2024-09-13 15:12:46 +0530

    MainActivity.java

---
+++ games/MainActivity.java
@@ -0,0 +1,18 @@
+package com.devtides.a2048clone;
+
+import ...
+
+
+
+
+public class MainActivity extends Activity
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  
+  requestWindowFeature(Window.FEATURE_NO_TITLE);
+  
+  setContenView(R.layout.activity_main);
+}
+}
